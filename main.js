227439e50cb6b3757c64d6a00ef3cf59615ababb
espadv69 <aitorjob69@gmail.com>
2025-01-25 19:07:37 +0100

    Add global variables for game state management in main.js

--- main.js
+++ main.js
@@ -2,3 +2,8 @@
 const $cardGrid = document.querySelector('.card-grid')
 const $movesCounter = document.querySelector('.moves-counter')
 const $restartBtn = document.querySelector('.restart-btn')
+
+// Global variables for the game
+let moves = 0 // Counter for the number of moves
+let flippedCards = [] // Cards currently flipped
+let matchedCards = 0 // Counter for matched pairs
