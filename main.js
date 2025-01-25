bb5efc2a6bbae513ebf000baaeeb60d12b2e5ec5
espadv69 <aitorjob69@gmail.com>
2025-01-25 19:04:12 +0100

    Add initial DOM element selections in main.js for game functionality

---
+++ main.js
@@ -0,0 +1,4 @@
+// DOM elements
+const $cardGrid = document.querySelector('.card-grid')
+const $movesCounter = document.querySelector('.moves-counter')
+const $restartBtn = document.querySelector('.restart-btn')
